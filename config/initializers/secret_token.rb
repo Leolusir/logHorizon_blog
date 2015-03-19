1eab14b00edf3f6fd0b0c581ef5b49856cdb72e3
thomas <luotao0124@gmail.com>
2015-03-19 15:43:17 +0800

    init commit with rails new pro & edit .gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogHorizonBlog::Application.config.secret_key_base = '6e99fb1ae9280f6781d3c9c37db20041fb5e1a23eb1fb98684c1c01bbeff8f147b41a5ffa200aedb252010a6f33e434b7659797d361eb72767a50e169cd0c77d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LogHorizonBlog::Application.config.session_store :cookie_store, key: '_logHorizon_blog_session'
