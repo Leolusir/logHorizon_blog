6fef65df614c786cf5b93669454bab06e55a68a2
Thomas Luo <luotao0124@gmail.com>
2015-03-20 10:45:28 +0800

    Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
