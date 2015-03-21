607973745d2e9f466d68dd32859f9e816813951d
Thomas Luo <luotao0124@gmail.com>
2015-03-21 21:36:34 +0800

    update config file with about route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ LogHorizonBlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/about', to: 'welcome#about'
 
   resources :articles
 
