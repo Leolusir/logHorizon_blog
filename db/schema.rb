589e59a95bd27ca3263df32d2882d4769b925f49
Thomas Luo <luotao0124@gmail.com>
2015-03-21 22:42:13 +0800

    new migrate to create articles_tags_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320163953) do
+ActiveRecord::Schema.define(version: 20150321144015) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "articles_tags", force: true do |t|
+    t.integer "article_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
