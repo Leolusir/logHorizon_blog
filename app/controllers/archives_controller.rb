edf9bf9c05077cfa95f0926c824358c5f104840e
Thomas Luo <luotao0124@gmail.com>
2015-03-28 20:14:02 +0800

    archive index

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,4 +2,15 @@ class ArchivesController < ApplicationController
   def index
     @articles_month = Article.all.group_by{ |a| a.created_at.beginning_of_year }
   end
+  # def index
+  #   @articles_year = Article.all.group_by{ |a| a.created_at.beginning_of_year }
+  #   @articles_array = []
+  #   if params[:month_id]
+  #     @articles_year.each do |year, a|
+  #       if year.beginning_of_year.year.to_s == params[:year_id]
+  #         @articles_result = a
+  #       end
+  #     end
+  #   end
+  # end
 end
