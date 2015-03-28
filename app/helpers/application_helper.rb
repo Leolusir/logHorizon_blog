c3ee7fd598bb1c6af1a9a0173abbb68159249a4a
Thomas Luo <luotao0124@gmail.com>
2015-03-28 20:49:09 +0800

    markdown syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # def markdown(text)
+    # Redcarpet.new(text).to_html.html_safe
+  # end
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    disable_indented_code_blocks: true)
+    return markdown.render(text).html_safe
+  end
+
 end
