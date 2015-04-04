21ed1cbac58fcb8358c0d4a972b5ef2c01a62613
Thomas Luo <luotao0124@gmail.com>
2015-04-05 04:21:42 +0800

    set max page parameter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:page]
+    page_count = Article.all.count / 5
+    if params[:page].to_i <= page_count
       @articles = Article.limit(5).offset(params[:page].to_i()*5)
     else
       @articles = Article.limit(5)
