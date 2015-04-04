a5718f23bbdf79bef112e35445a977432fd9b7bc
Thomas Luo <luotao0124@gmail.com>
2015-04-05 01:14:51 +0800

    pagination(to be review)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.all
+    if params[:page]
+      @articles = Article.limit(5).offset(params[:page].to_i()*5)
+    else
+      @articles = Article.limit(5)
+    end
   end
   def about
   end
