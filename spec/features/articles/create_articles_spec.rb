186b6db943d4bf08ce4189f55f63ebe48a37c78a
Thomas Luo <luotao0124@gmail.com>
2015-04-04 21:40:27 +0800

    add some test case

--- spec/features/articles/create_articles_spec.rb
+++ spec/features/articles/create_articles_spec.rb
@@ -5,7 +5,7 @@ feature 'articles' do
     create(:user, email: 'person@example.com')
   end
 
-  scenario '#new' do
+  scenario 'created with user logged in and valid infomation' do
     sign_in 
     visit(new_article_path)
     within("#new_article") do
@@ -16,4 +16,48 @@ feature 'articles' do
     click_button 'Create Article'
     expect(page).to have_content('successfully')
   end
+
+  scenario 'create failed with user have not logged' do
+    sign_in(password:'wrongpass')
+    visit(new_article_path)
+    expect(page).to have_content('You need to sign in or sign up')
+  end
+
+  scenario 'create failed with title length < 5 character' do
+    sign_in
+    visit(new_article_path)
+    within("#new_article") do
+      fill_in 'Title', :with => 'Test'
+      fill_in 'Tags', :with => 'test; tags'
+      fill_in 'Content', :with => 'Hello, world! This line is post by Capybara.'
+    end
+    click_button 'Create Article'
+    expect(page).to have_content('Title is too short')
+  end
+
+  scenario 'create failed with content is nil' do
+    sign_in
+    visit(new_article_path)
+    within("#new_article") do
+      fill_in 'Title', :with => 'Test is test'
+      fill_in 'Tags', :with => 'test; tag'
+      fill_in 'Content', :with => ''
+    end
+    click_button 'Create Article'
+    expect(page).to have_content("Content can't be blank")
+  end
+
+  scenario 'created tags successfully' do
+    sign_in
+    visit(new_article_path)
+    within("#new_article") do
+      fill_in 'Title', :with => 'Test article'
+      fill_in 'Tags', :with => 'test; tag'
+      fill_in 'Content', :with => 'Here is a content. : )'
+    end
+    click_button 'Create Article'
+    expect(page).to have_css('a.ui.tag.label')
+    expect(page).to have_content('test')
+  end
+
 end
