f5c41f945f7b366cdc4254a678dc2e1bbbae2d79
Thomas Luo <luotao0124@gmail.com>
2015-04-05 23:36:38 +0800

    [feature] archive by date

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,15 +2,21 @@ class ArchivesController < ApplicationController
   def index
     @articles_month = Article.all.group_by{ |a| a.created_at.beginning_of_year }
   end
-  # def index
-  #   @articles_year = Article.all.group_by{ |a| a.created_at.beginning_of_year }
-  #   @articles_array = []
-  #   if params[:month_id]
-  #     @articles_year.each do |year, a|
-  #       if year.beginning_of_year.year.to_s == params[:year_id]
-  #         @articles_result = a
-  #       end
-  #     end
-  #   end
-  # end
+  def show
+    if params[:month_id] && params[:year_id]
+      @articles_month = Article.all.group_by{|a| a.created_at.beginning_of_month}
+      @articles_month.each do |month, array|
+        if month.year.to_s == params[:year_id] && month.month.to_s == params[:month_id]
+          @result = array
+        end
+      end
+    elsif params[:year_id]
+      @articles_year = Article.all.group_by{|a| a.created_at.beginning_of_year}
+      @articles_year.each do |year, array|
+        if year.year.to_s == params[:year_id]
+          @result = array
+        end
+      end
+    end
+  end
 end
