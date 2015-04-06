c2e96a1fd281afd7739fa5a5048e6818942cd161
Thomas Luo <luotao0124@gmail.com>
2015-04-06 17:27:18 +0800

    add article count logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    count = @article.count
+    @article.update_columns(count: count+1)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true
   has_and_belongs_to_many :tags
+
+  after_save do |article|
+    article.update_columns(count: 0)
+  end
 end
