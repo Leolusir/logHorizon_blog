21f8de3f33c381dc9e055376cfed9251de65c520
Thomas Luo <luotao0124@gmail.com>
2015-04-20 14:45:10 +0800

    [chore] use double-quoted

--- spec/model/tag_spec.rb
+++ spec/model/tag_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Tag, :type => :model do
-  it 'is valid with a valid name' do
+  it "is valid with a valid name" do
     tag = build(:tag)
     expect(tag).to be_valid
   end
 
-  it 'is invalid with a nil name' do
+  it "is invalid with a nil name" do
     tag = build(:tag, name: nil)
     tag.valid?
     expect(tag.errors[:name]).to include("can't be blank")
   end
 
-  it 'is invalid with a short name' do
+  it "is invalid with a short name" do
     tag = build(:tag, name: 'name')
     tag.valid?
     expect(tag.errors[:name]).to include("is too short (minimum is 5 characters)")
